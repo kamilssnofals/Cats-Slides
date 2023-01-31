5df0d7154007f4897ab37ce41df0734859f867a1
Your Name <you@example.com>
2023-01-31 16:09:33 +0300

    Cats-Slides

---
+++ script.js
@@ -0,0 +1,39 @@
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sideBar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const container = document.querySelector('.container')
+const slidesCount = mainSlide.querySelectorAll('div').length
+
+
+sideBar.style.top = `-${(slidesCount - 1) * 100}vh`
+let activeSlidesIndex = 0
+upBtn.addEventListener('click', () => {
+  changeSlides('up')
+})
+
+downBtn.addEventListener('click',() => {
+  changeSlides('down')
+})
+
+function changeSlides(direction) {
+  if(direction === 'up') {
+    activeSlidesIndex++
+    if(activeSlidesIndex === slidesCount) {
+      activeSlidesIndex = 0
+    }
+  } else if (direction === 'down') {
+    activeSlidesIndex--
+    if(activeSlidesIndex < 0) {
+      activeSlidesIndex = slidesCount - 1 
+    }
+  } 
+  const height = container.clientHeight
+  mainSlide.style.transform = `translateY(-${activeSlidesIndex * height}px)`
+  sideBar.style.transform = `translateY(${activeSlidesIndex * height}px)`
+}
+
+
+
+
+
